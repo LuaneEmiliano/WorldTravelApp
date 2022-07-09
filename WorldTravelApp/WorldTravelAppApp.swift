d7c898b10815417743e74866177118bcec1de351
luane <luaneemiliano@gmail.com>
2022-07-09 08:10:58 -0700

    Initial Commit

---
+++ WorldTravelApp/WorldTravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTravelAppApp.swift
+//  WorldTravelApp
+//
+//  Created by luane Niejelski on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldTravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
