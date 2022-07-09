0478e6f550bf2f513e2f4a559853fbd825d8c26a
luane <luaneemiliano@gmail.com>
2022-07-09 09:32:06 -0700

    -Set up map -Update map location

--- ViewModels/LocationsViewModel.swift
+++ ViewModels/LocationsViewModel.swift
@@ -6,13 +6,34 @@
 //
 
 import Foundation
+import MapKit
 
 class LocationsViewModel: ObservableObject {
     
+    //All loaded locations
     @Published var locations: [Location]
+    
+    //Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    //latitudeDelta and longitudeDelta is how zoom in and how zoom out you want your map to be.
+    let mapSpam = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        mapRegion = MKCoordinateRegion(center: location.coordinates, span:  mapSpam)
     }
 }
